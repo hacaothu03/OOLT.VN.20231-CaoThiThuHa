606ce2d2007a3e785ecf66791b7c8ec3720d4004
Ha Cao Thu <ha.caothu.03@gmail.com>
2023-10-29 13:56:50 +0700

    fix

---
+++ lab01/HelloWorld1.java
@@ -0,0 +1,8 @@
+package lab01;
+//lab01: exercise 2.2.1
+public class HelloWorld1 {
+	public static void main(String args[]) {
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+	}
+}
