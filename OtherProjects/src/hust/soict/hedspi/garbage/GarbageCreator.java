96a8d56366c8fb0a88f46d427e9eba5f63a073f8
Ha Cao Thu <ha.caothu.03@gmail.com>
2023-11-26 18:34:33 +0700

    add OtherProjects/lab03

--- OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
+++ OtherProjects/src/hust/soict/hedspi/garbage/GarbageCreator.java
@@ -1,5 +1,24 @@
 package hust.soict.hedspi.garbage;
 
-public class GarbageCreator {
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "E:\\_2020-06-22-1426-8707-de_inferno--vs-.dem";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        startTime = System.currentTimeMillis();
+        String outputString = "";
+        for (byte b : inputBytes) {
+            outputString += (char) b;
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time elapsed when using String: " + (endTime - startTime) + "ms");
+    }
 }
--- OtherProjects/src/hust/soict/hedspi/garbage/NoGarbage.java
+++ OtherProjects/src/hust/soict/hedspi/garbage/NoGarbage.java
@@ -1,5 +1,24 @@
 package hust.soict.hedspi.garbage;
 
-public class NoGarbage {
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class NoGarbage {
+    public static void main(String[] args) {
+        String filename = "E:\\_2020-06-22-1426-8707-de_inferno--vs-.dem";
+        byte[] inputBytes = { 0 };
+        long startTime, endTime;
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        startTime = System.currentTimeMillis();
+        StringBuilder outputString = new StringBuilder();
+        for (byte b : inputBytes) {
+            outputString.append((char) b);
+        }
+        endTime = System.currentTimeMillis();
+        System.out.println("Time elapsed when using StringBuilder: " + (endTime - startTime) + "ms");
+    }
 }
