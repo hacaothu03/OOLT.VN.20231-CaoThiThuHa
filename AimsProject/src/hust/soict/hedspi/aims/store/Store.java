088a3bcd19a9a1c9b414eda5f4ea24d1cbd1fb10
Ha Cao Thu <ha.caothu.03@gmail.com>
2023-11-26 18:24:53 +0700

    update

--- AimsProject/src/hust/soict/hedspi/aims/store/Store.java
+++ AimsProject/src/hust/soict/hedspi/aims/store/Store.java
@@ -1,28 +1,46 @@
 package hust.soict.hedspi.aims.store;
 
-import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.*;
+import java.util.*;
 
 public class Store {
-    private DigitalVideoDisc itemsInStore[] = new DigitalVideoDisc[65536];
+    private List<Media> itemsInStore = new ArrayList<Media>();
 
-    public void addDVD(DigitalVideoDisc disc) {
-        int index = itemsInStore.length - 1;
-        itemsInStore[index] = disc;
+    public void addMedia(Media media) {
+        itemsInStore.add(media);
     }
 
-    public void removeDVD(DigitalVideoDisc disc) {
-        DigitalVideoDisc itemsInStoreRemoved[] = new DigitalVideoDisc[itemsInStore.length - 1];
-        int j = 0;
-        for (int i = 0; i < itemsInStore.length; ++i) {
-            if (itemsInStore[i] != disc) {
-                itemsInStoreRemoved[j] = itemsInStore[i];
-                j++;
-            }
-        }
-        itemsInStore = itemsInStoreRemoved;
+    public void removeMedia(Media media) {
+        itemsInStore.remove(media);
     }
 
-    public DigitalVideoDisc[] getItemsInStore() {
+    public List<Media> getItemsInStore() {
         return itemsInStore;
     }
+
+    public Media search(String title) {
+        for (Media media : itemsInStore) {
+            if (media.getTitle().equals(title)) {
+                return media;
+            }
+        }
+        return null;
+    }
+
+    public void printStore() {
+        if (itemsInStore.size() == 0) {
+            System.out.println("The store is empty!");
+        } else {
+            System.out.println("********************STORE INVENTORY********************");
+            for (Media media : itemsInStore) {
+                System.out.println(media);
+            }
+            System.out.println("********************************************************");
+        }
+    }
+
+	public void addDVD(DigitalVideoDisc dvd2) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
+++ AimsProject/src/hust/soict/hedspi/test/disc/TestPassingParameter.java
@@ -1,31 +1,44 @@
 package hust.soict.hedspi.test.disc;
 
-import hust.soict.hedspi.aims.disc.DigitalVideoDisc;
+import hust.soict.hedspi.aims.media.DigitalVideoDisc;
+
+class DvdWrapper {
+    DigitalVideoDisc obj;
+
+    DvdWrapper(DigitalVideoDisc disc) {
+        this.obj = disc;
+    }
+}
 
 public class TestPassingParameter {
+    public static void main(String[] args) {
+        DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+        DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
 
-	public static void swap(Object o1, Object o2) {
-		Object temp = o1;
-		o1 = 02; 
-		o2 = temp;
-	}
-	public static void changeTitle(DigitalVideoDisc dvd, String title) {
-		String oldTitle = dvd.getTitle();
-		dvd.setTitle(title);
-		dvd = new DigitalVideoDisc(oldTitle);
-	}
-	public static void main(String[] args) {
+        DvdWrapper jungle = new DvdWrapper(jungleDVD);
+        DvdWrapper cinderella = new DvdWrapper(cinderellaDVD);
 
-		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
-		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
-		
-		swap(jungleDVD, cinderellaDVD);
-		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
-		System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
-		
-		changeTitle(jungleDVD, cinderellaDVD.getTitle());
-		System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+        System.out.println("cinderella dvd title: " + cinderellaDVD.getTitle());
 
-	}
+        swap(jungle, cinderella);
 
+        jungleDVD = jungle.obj;
+        cinderellaDVD = cinderella.obj;
+
+        // changeTitle(jungleDVD, cinderellaDVD.getTitle());
+        System.out.println("jungle dvd title: " + jungleDVD.getTitle());
+    }
+
+    public static void swap(DvdWrapper o1, DvdWrapper o2) {
+        DigitalVideoDisc tmp = o1.obj;
+        o1.obj = o2.obj;
+        o2.obj = tmp;
+    }
+
+    public static void changeTitle(DigitalVideoDisc dvd, String title) {
+        String oldTitle = dvd.getTitle();
+        dvd.setTitle(title);
+        dvd = new DigitalVideoDisc(oldTitle);
+    }
 }
