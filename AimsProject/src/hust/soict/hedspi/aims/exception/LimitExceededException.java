ab52c0976654c1b64746157074ff6eb14b7731f0
Ha Cao Thu <ha.caothu.03@gmail.com>
2023-12-21 17:11:13 +0700

    add exception

--- AimsProject/src/hust/soict/hedspi/aims/exception/LimitExceededException.java
+++ AimsProject/src/hust/soict/hedspi/aims/exception/LimitExceededException.java
@@ -1,5 +1,8 @@
 package hust.soict.hedspi.aims.exception;
 
-public class LimitExceededException {
-
+public class LimitExceededException extends Exception {
+	
+	public LimitExceededException(String string) {
+		super(string);
+	}
 }
--- AimsProject/src/hust/soict/hedspi/aims/exception/PlayerException.java
+++ AimsProject/src/hust/soict/hedspi/aims/exception/PlayerException.java
@@ -1,5 +1,8 @@
 package hust.soict.hedspi.aims.exception;
 
-public class PlayerException {
-
+public class PlayerException extends Exception {
+	
+	public PlayerException(String string) {
+		super(string);
+	}
 }
